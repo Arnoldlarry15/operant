650c7255656970e1eecf24409fead0afb9e88668
Arnoldlarry15 <arnoldlarry15@gmail.com>
2026-06-25 19:45:57 -0500

    auth debugging

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const dynamic = 'force-dynamic'
+
 import { Suspense } from 'react'
 import { useAppState } from '@/lib/app-state'
 import { Navbar } from '@/components/navbar'
